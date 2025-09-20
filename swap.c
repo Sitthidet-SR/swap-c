89b6386c4f4268c74249caf79f4e61b323ec1ba3
Sitthidet-SR <Sitthidet.SR@gmail.com>
2025-09-20 17:14:28 +0700

    1

--- swap.c
+++ swap.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
+int swap(int *a, int *b, int *c, int *d) {
+    int temp = *a;
+    *a = *b;
+    *b = *c;
+    *c = *d;
+    *d = temp;
+}
+
 int main() {
-    printf("Hello, World!\n");
+    int w, x, y, z;
+    printf("Input data 4 digit: \n");
+    scanf("%d %d %d %d", &w, &x, &y, &z);
+    printf("Before Swap: w = %d, x = %d, y = %d, z = %d \n", w, x, y, z); //Before swap
+    swap(&w, &x, &y, &z);
+    printf("After Swap:  w = %d, x = %d, y = %d, z = %d \n", w, x, y, z); //After swap
     return 0;
 }
